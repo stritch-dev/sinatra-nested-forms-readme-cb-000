0f7faf21c77c48befff2659660d18f63b2a93fd9
stritch.dev <stritch.dev@gmail.com>
2018-03-28 12:04:14 -0500

    Done.

---
+++ models/student.rb
@@ -0,0 +1,18 @@
+
+class Student
+  attr_reader :name, :grade
+   
+   STUDENTS = []
+    
+   def initialize(params)
+       @name = params[:name]
+       @grade = params[:grade]
+       STUDENTS << self
+   end
+      
+   def self.all
+     STUDENTS
+   end
+        
+end
+
